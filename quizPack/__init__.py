3e64b07156f005e96f95ae18a820005249ca973e
hemakesh-github <hemakesh8333@gmail.com>
2024-05-23 09:13:48 +0000

    Modified for deployment

--- quizPack/__init__.py
+++ quizPack/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 import os
+import json
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/HEMAKESH/Documents/backend/Quiz/wiseUp.db'
+with open('/etc/config.json') as config_file:
+	config = json.load(config_file)
+
+app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/ec2-user/wiseUp/wiseUp.db'
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
